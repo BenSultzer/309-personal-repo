c5004455fc657c99e58b14abdabb8eebd81ae31a
BenSultzer <bms3902@rit.edu>
2023-09-14 21:32:23 -0400

    Almost done checking E03.

--- Exercises/E03-Ben-Sultzer/E03-Ben-Sultzer.cpp
+++ Exercises/E03-Ben-Sultzer/E03-Ben-Sultzer.cpp
@@ -42,11 +42,21 @@ void DrawTree() {
     glColor3f(0.0f, 1.0f, 0.0f);
     glBegin(GL_TRIANGLES);
     for (int i = 0; i < 7; i++) {
-        // Seven triangles with a right base point at half-x coordinates between
-        // 5.0 and 10.0 and a left base point at half-x coordinates between 
-        // 0.0 and 5.0.
+        // 7 triangles with evenly incremented base lengths and vertical positions
+        // to create a tapering tree structure
+        // Left base vertex x-coordinate is increased by 0.5 each
+        // iteration going towards the center and starting at 1.0. The y-
+        // coordinate is increased by 0.5 each iteration starting at 2.5
         glVertex2f(1.0 + ((5.0 / 10.0) * i), 2.5 + ((5.0 / 10.0) * i));
+
+        // Top vertex x-coordinate constantly stays in the center at 5.0 
+        // and the y-coordinate is incremented by 0.5 each iteration starting
+        // at 3.5 
         glVertex2f(5.0f, 3.5 + ((5.0 / 10.0) * i));
+
+        // Right base vertex x-coordinate is decreased by 0.5 each
+        // iteration going towards the center and starting at 9.0. The y-
+        // coordinate is increased by 0.5 each iteration starting at 2.5.
         glVertex2f(9.0 - ((5.0 / 10.0) * i), 2.5 + ((5.0 / 10.0) * i));
     }
     glEnd();
@@ -68,26 +78,29 @@ void DrawTree() {
     glEnd();
 }
 
-// called when the GL context need to be rendered
+/// <summary>
+/// Called when the GL context needs to be rendered
+/// </summary>
+/// <param name="">Takes no parameters</param>
 void display(void)
 {
-    // clear the screen to white, which is the background color
+    // Clear the screen to white, which is the background color
     glClearColor(1.0, 1.0, 1.0, 0.0);
 
-    // clear the buffer stored for drawing
+    // Clear the buffer stored for drawing
     glClear(GL_COLOR_BUFFER_BIT);
 
 
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
 
-    // specify the color for drawing
+    // Specify the color for drawing
     glColor3f(1.0, 0.0, 0.0);
 
-    // this is immedidate mode of OpenGL usded prior to OpenGL 3.0
+    // This is the immediate mode of OpenGL used prior to OpenGL 3.0
     DrawTree();
 
-    // specify the color for new drawing
+    // Specify the color for new drawing
     glColor3f(0.0, 0.0, 1.0);
 
     // Reset the point size
@@ -96,20 +109,24 @@ void display(void)
     glutSwapBuffers();
 }
 
-// called when window is first created or when window is resized
+/// <summary>
+/// Called when window is first created or when window is resized
+/// </summary>
+/// <param name="w">The width of the window</param>
+/// <param name="h">The height of the window</param>
 void reshape(int w, int h)
 {
-    // update thescreen dimensions
+    // Update the screen dimensions
     width = w;
     height = h;
 
-    //do an orthographic parallel projection, limited by screen/window size
+    // Do an orthographic parallel projection, limited by screen/window size
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     gluOrtho2D(0.0, 10.0, 0.0, 10.0);
     //gluOrtho2D(-5.0, 5.0, -5.0, 5.0);
 
-    /* tell OpenGL to use the whole window for drawing */
+    /* Tell OpenGL to use the whole window for drawing */
     glViewport(0, 0, (GLsizei)width, (GLsizei)height);
     //glViewport((GLsizei) width/2, (GLsizei) height/2, (GLsizei) width, (GLsizei) height);
 
@@ -118,30 +135,26 @@ void reshape(int w, int h)
 
 int main(int argc, char* argv[])
 {
-    // Set the random number generator for random ornament colors
-    srand(time(0));
-
-    // before create a glut window,
-    // initialize stuff not opengl/glut dependent
+    // Before creating a glut window, initialize stuff not opengl/glut dependent
     init();
 
-    //initialize GLUT, let it extract command-line GLUT options that you may provide
-    //NOTE that the '&' before argc
+    // Initialize GLUT, let it extract command-line GLUT options that you may provide
+    // NOTE the '&' comes before argc
     glutInit(&argc, argv);
 
-    // specify as double bufferred can make the display faster
-    // Color is speicfied to RGBA, four color channels with Red, Green, Blue and Alpha(depth)
+    // Specifying double buffered can make the display faster
+    // Color is specified to RGBA, four color channels with Red, Green, Blue, and Alpha (depth)
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
 
-    //set the initial window size */
+    // Set the initial window size
     glutInitWindowSize((int)width, (int)height);
 
-    // create the window with a title
+    // Create the window with a title
     glutCreateWindow("First OpenGL Program");
 
-    /* --- register callbacks with GLUT --- */
+    /* --- Register callbacks with GLUT --- */
 
-    //register function to handle window resizes
+    // Register function to handle window resizing
     glutReshapeFunc(reshape);
 
     //register function that draws in the window
