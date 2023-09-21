1251b72e7ea408e78e339eb1a81e47761950310f
BenSultzer <bms3902@rit.edu>
2023-09-20 23:33:37 -0400

    A01 complete!

--- Exercises/A01-Ben-Sultzer/A01-Ben-Sultzer.cpp
+++ Exercises/A01-Ben-Sultzer/A01-Ben-Sultzer.cpp
@@ -9,7 +9,6 @@
 #endif
 
 #include <math.h>
-#include <iostream>
 
 using namespace std;
 
@@ -87,62 +86,94 @@ void drawWireframeCircle(float red, float green, float blue, float center_x, flo
     glLineWidth(1.0f);
 }
 
+/// <summary>
+/// Draws a cartoon panda using the drawFilledCircle() and drawWireframeCircle()
+/// functions
+/// </summary>
+void drawPanda() {
+    // Back and front leg (behind body)
+    drawFilledCircle(0.0f, 0.0f, 0.0f, 5.0f, 2.0f, 0.9f);
+    drawFilledCircle(0.0f, 0.0f, 0.0f, 9.0f, 2.0f, 0.9f);
+
+    // Tail
+    drawFilledCircle(0.0f, 0.0f, 1.0f, 4.0f, 8.0f, 0.75f);
+    drawWireframeCircle(0.0f, 0.0f, 0.0f, 4.0f, 8.0f, 0.75f, 2.0f);
+
+    // Body
+    drawFilledCircle(0.0f, 0.0f, 1.0f, 6.5f, 5.0f, 3.5f);
+
+    // Back and front leg (in front of body)
+    drawFilledCircle(0.0f, 0.0f, 0.0f, 4.0f, 2.0f, 0.9f);
+    drawFilledCircle(0.0f, 0.0f, 0.0f, 8.0f, 2.0f, 0.9f);
+
+    // Ear (behind head)
+    drawFilledCircle(0.0f, 0.0f, 0.0f, 12.0f, 8.0f, 0.9f);
+    drawWireframeCircle(0.0f, 0.0f, 0.0f, 12.0f, 8.0f, 0.9f, 2.0f);
+
+    // Head
+    drawFilledCircle(0.0f, 0.0f, 1.0f, 10.0f, 5.5f, 2.75f);
+    drawWireframeCircle(0.0f, 0.0f, 0.0f, 10.0f, 5.5f, 2.75f, 2.0f);
+
+    // Ear (in front of head)
+    drawFilledCircle(0.0f, 0.0f, 0.0f, 8.0f, 8.0f, 0.9f);
+    drawWireframeCircle(0.0f, 0.0f, 0.0f, 8.0f, 8.0f, 0.9f, 2.0f);
+
+    // Horn
+    drawFilledCircle(1.0f, 0.75f, 0.0f, 10.0f, 8.0f, 0.6f);
+    drawWireframeCircle(0.0f, 0.0f, 0.0f, 10.0f, 8.0f, 0.6f, 2.0f);
+    drawFilledCircle(1.0f, 0.75f, 0.0f, 10.25f, 8.15f, 0.5f);
+    drawWireframeCircle(0.0f, 0.0f, 0.0f, 10.25f, 8.15f, 0.5f, 2.0f);
+    drawFilledCircle(1.0f, 0.75f, 0.0f, 10.5f, 8.3f, 0.4f);
+    drawWireframeCircle(0.0f, 0.0f, 0.0f, 10.5f, 8.3f, 0.4f, 2.0f);
+    drawFilledCircle(1.0f, 0.75f, 0.0f, 10.75f, 8.45f, 0.3f);
+    drawWireframeCircle(0.0f, 0.0f, 0.0f, 10.75f, 8.45f, 0.3f, 2.0f);
+    drawFilledCircle(1.0f, 0.75f, 0.0f, 11.0f, 8.6f, 0.2f);
+    drawWireframeCircle(0.0f, 0.0f, 0.0f, 11.0f, 8.6f, 0.2f, 2.0f);
+
+    // Eyes (pupil)
+    drawFilledCircle(0.0f, 0.0f, 0.0f, 10.0f, 5.75f, 0.4f);
+    drawFilledCircle(0.0f, 0.0f, 0.0f, 11.0f, 5.75f, 0.4f);
+
+    // Eyes (highlights)
+    drawFilledCircle(1.0f, 1.0f, 1.0f, 10.15f, 5.7f, 0.15f);
+    drawFilledCircle(1.0f, 1.0f, 1.0f, 11.15f, 5.7f, 0.15f);
+    drawFilledCircle(1.0f, 1.0f, 1.0f, 9.85f, 5.9f, 0.1f);
+    drawFilledCircle(1.0f, 1.0f, 1.0f, 10.85f, 5.9f, 0.1f);
+
+    // Nose
+    drawFilledCircle(0.0f, 0.0f, 0.0f, 10.5f, 5.0f, 0.2f);
+    drawFilledCircle(0.0f, 0.0f, 0.0f, 10.25f, 5.0f, 0.2f);
+    drawFilledCircle(0.0f, 0.0f, 0.0f, 10.75f, 5.0f, 0.2f);
+    drawFilledCircle(0.0f, 0.0f, 0.0f, 10.5f, 4.75f, 0.2f);
+
+    // Mouth
+    drawFilledCircle(0.0f, 0.0f, 0.0f, 10.5f, 4.25f, 0.35f);
+
+    // Spots
+    drawFilledCircle(1.0f, 0.92f, 0.0f, 4.5f, 4.75f, 1.0f);
+    drawFilledCircle(1.0f, 0.92f, 0.0f, 6.5f, 7.0f, 0.45f);
+    drawFilledCircle(1.0f, 0.0f, 0.0f, 6.75f, 4.75f, 0.35f);
+    drawFilledCircle(1.0f, 0.0f, 0.0f, 12.0f, 5.0f, 0.45f);
+    drawFilledCircle(1.0f, 0.0f, 1.0f, 8.5f, 5.5f, 0.6f);
+    drawFilledCircle(0.0f, 0.92f, 1.0f, 6.0f, 2.5f, 0.5f);
+    drawFilledCircle(1.0f, 0.65f, 0.0f, 4.25f, 8.0f, 0.25f);
+}
+
+/// <summary>
+/// Initialize program-specific members
+/// </summary>
+/// <param name="">Takes no parameters</param>
 void init(void)
 {
     // initialize the size of the window
-    width = 600;
-    height = 600;
+    width = 854;
+    height = 480;
 }
 
-void DrawTank() {
-    // this is immedidate mode of OpenGL usded prior to OpenGL 3.0
-    // draw hull
-    glColor3f(0.5f, 0.75f, 0.3f);
-    glBegin(GL_TRIANGLE_STRIP);
-    glVertex2f(4.0f, 3.75f);
-    glVertex2f(4.0f, 6.25f);
-    glVertex2f(6.0f, 3.75f);
-    glVertex2f(6.0f, 6.25f);
-    glEnd();
-
-    // draw turret
-    glColor3f(0.75f, 0.3f, 0.1f);
-    glBegin(GL_TRIANGLE_STRIP);
-    glVertex2f(4.35f, 4.25f);
-    glVertex2f(4.35f, 5.75f);
-    glVertex2f(5.65f, 4.25f);
-    glVertex2f(5.65f, 5.75f);
-    glEnd();
-
-    // draw gun
-    glColor3f(0.5f, 0.3f, 0.1f);
-    glBegin(GL_TRIANGLE_STRIP);
-    glVertex2f(4.8f, 5.75f);
-    glVertex2f(4.8f, 7.00f);
-    glVertex2f(5.2f, 5.75f);
-    glVertex2f(5.2f, 7.00f);
-    glEnd();
-
-    // draw left caterpillar tracks
-    glColor3f(0.3f, 0.3f, 0.3f);
-    glBegin(GL_TRIANGLE_STRIP);
-    glVertex2f(3.5f, 3.5f);
-    glVertex2f(3.5f, 6.5f);
-    glVertex2f(4.0f, 3.5f);
-    glVertex2f(4.0f, 6.5f);
-    glEnd();
-
-    // draw right caterpillar tracks
-    glColor3f(0.3f, 0.3f, 0.3f);
-    glBegin(GL_TRIANGLE_STRIP);
-    glVertex2f(6.0f, 3.5f);
-    glVertex2f(6.0f, 6.5f);
-    glVertex2f(6.5f, 3.5f);
-    glVertex2f(6.5f, 6.5f);
-    glEnd();
-}
-
-// called when the GL context need to be rendered
+/// <summary>
+/// Called when the GL context needs to be rendered
+/// </summary>
+/// <param name="">Takes no parameters</param>
 void display(void)
 {
     // clear the screen to white, which is the background color
@@ -158,32 +189,30 @@ void display(void)
     // specify the color for drawing
     glColor3f(1.0, 0.0, 0.0);
 
-    drawFilledCircle(0.5f, 0.5f, 0.5f, 5.0f, 5.0f, 2.0f);
+    // Draw the panda
+    drawPanda();
 
     // specify the color for new drawing
     glColor3f(0.0, 0.0, 1.0);
 
-    // draw the origin of the canvas
-    glPointSize(30.0f);
-    glBegin(GL_POINTS);
-    glVertex2f(0.0f, 0.0f);
-    glEnd();
-    glPointSize(1.0f);
-
     glutSwapBuffers();
 }
 
-// called when window is first created or when window is resized
+/// <summary>
+/// Called when window is first created or when window is resized
+/// </summary>
+/// <param name="w">The window width</param>
+/// <param name="h">The window height</param>
 void reshape(int w, int h)
 {
-    // update thescreen dimensions
+    // update the screen dimensions
     width = w;
     height = h;
 
     //do an orthographic parallel projection, limited by screen/window size
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    gluOrtho2D(0.0, 10.0, 0.0, 10.0);
+    gluOrtho2D(0.0, 16.0, 0.0, 9.0);
     //gluOrtho2D(-5.0, 5.0, -5.0, 5.0);
 
     /* tell OpenGL to use the whole window for drawing */
@@ -198,20 +227,25 @@ void reshape(int w, int h)
 /// of the character by increasing or decreasing the number of vertices 
 /// to draw per circle
 /// </summary>
-/// <param name="key">The key that was pressed</param>
+/// <param name="key">The ASCII code of the key that was pressed</param>
 /// <param name="x">the x-coordinate of the mouse position at the time
 /// the key was pressed</param>
 /// <param name="y">The y-coordinate of the mouse position at the time
 /// the key was pressed</param>
 void keyboard(unsigned char key, int x, int y) {
-    if (key == GLUT_KEY_UP) {
+    // When the user presses the "plus" key, add more vertices. The maximum
+    // number of vertices is 100
+    if ((key == 43) && (numVertices < 100)) {
         numVertices++;
     }
-    else if (key == GLUT_KEY_DOWN) {
+    // When the user presses the "minus" key, subtract vertices. The minimum
+    // number of vertices is 3
+    else if ((key == 45) && (numVertices > 3)) {
         numVertices--;
     }
 
-    cout << numVertices << endl;
+    // Re-render the window
+    glutPostRedisplay();
 }
 
 int main(int argc, char* argv[])
@@ -232,7 +266,7 @@ int main(int argc, char* argv[])
     glutInitWindowSize((int)width, (int)height);
 
     // create the window with a title
-    glutCreateWindow("First OpenGL Program");
+    glutCreateWindow("A01-Ben-Sultzer");
 
     /* --- register callbacks with GLUT --- */
 
