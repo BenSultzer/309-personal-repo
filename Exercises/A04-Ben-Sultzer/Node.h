548228d70edeb4c7d842f7b778acfa07a4507675
BenSultzer <bms3902@rit.edu>
2023-11-09 20:39:23 -0500

    Started defining Tree data structure.

---
+++ Exercises/A04-Ben-Sultzer/Node.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <string>
+
+using namespace std;
+
+/// <summary>
+/// Defines a node in the Tree structure that represents a robot's body
+/// </summary>
+class Node {
+private:
+	string bodyPart;			// The body part this Node represents
+	Node* left = nullptr;		// The left child of this Node
+	Node* center = nullptr;		// The center child of this Node
+	Node* right = nullptr;		// The right child of this Node
+
+public:
+	/// <summary>
+	/// The Node's constructor
+	/// </summary>
+	/// <param name="p_bodyPart">The name of the body part this
+	/// Node will represent</param>
+	Node(string p_bodyPart) {
+		bodyPart = p_bodyPart; // MUST BE DEFINED IN SRC FILE!!!
+	}
+};
